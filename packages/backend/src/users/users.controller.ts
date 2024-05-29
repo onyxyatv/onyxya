ca84296d831f68097da4bedf12f2b1a8622401c0
L4p1s <124094769+PommePain@users.noreply.github.com>
2024-05-29 21:53:02 +0200

    some tests on zod schema

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res, UsePipes } from '@nestjs/common';
+import { LoginSchema, LoginSchemaDto } from '@common/validation/auth/login.schema';
+import { Response } from 'express';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+    @Post('login')
+    @UsePipes()
+    login(@Body() body: LoginSchemaDto, @Res() res: Response): object {
+        const check = LoginSchema.safeParse(body);
+        if (!check.success) return res.status(HttpStatus.BAD_REQUEST).json(check.error);
+        return {};
+    }
+}
--- packages/common/validation/auth/login.schema.ts
+++ packages/common/validation/auth/login.schema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
-import { usernameMaxLength, passwordMinLength, passwordMaxLength } from '../json/user.schema.json';
+import { usernameMaxLength, passwordMinLength, passwordMaxLength } from '@common/schemaConstants';
 
 export const LoginSchema = z.object({
     username: z.string().min(4).max(usernameMaxLength),
     password: z.string().min(passwordMinLength).max(passwordMaxLength)
 });
+
+export type LoginSchemaDto = z.infer<typeof LoginSchema>;
