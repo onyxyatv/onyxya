19691aaa2fbf4c2e450e36351f7667cf0ecf7f24
L4p1s <124094769+PommePain@users.noreply.github.com>
2024-05-30 11:43:33 +0200

    ZodPipe added

--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, HttpStatus, Post, Res, UsePipes } from '@nestjs/common';
 import { LoginSchema, LoginSchemaDto } from '@common/validation/auth/login.schema';
 import { Response } from 'express';
+import { ZodValidationPipe } from 'src/pipes/zod.pipe';
 
 @Controller('users')
 export class UserController {
     @Post('login')
-    @UsePipes()
+    @UsePipes(new ZodValidationPipe(LoginSchema))
     login(@Body() body: LoginSchemaDto, @Res() res: Response): object {
         const check = LoginSchema.safeParse(body);
         if (!check.success) return res.status(HttpStatus.BAD_REQUEST).json(check.error);
-        return {};
+        return res.status(200).json({ success: true });
     }
 }
