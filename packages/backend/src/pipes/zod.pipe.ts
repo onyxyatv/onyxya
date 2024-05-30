f5a717ac48e230504bebf38389c7d220252ed69d
Enzo <118602062+enzomoy@users.noreply.github.com>
2024-05-30 15:13:37 +0200

    added custom error & custom zode pipe

--- packages/backend/src/pipes/zod.pipe.ts
+++ packages/backend/src/pipes/zod.pipe.ts
@@ -1,5 +1,6 @@
-import { PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
 import { ZodSchema } from 'zod';
+import { BadRequestError } from "@common/errors/CustomError";
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) { }
@@ -9,7 +10,10 @@ export class ZodValidationPipe implements PipeTransform {
       const parsedValue = this.schema.parse(value);
       return parsedValue;
     } catch (error) {
-      throw new BadRequestException('Validation failed');
+      const err = error.errors[0].message.split(" ");
+      err[0] = error.errors[0].path[0];
+      const str = err.join(" ");
+      throw new BadRequestError(str);
     }
   }
 }
