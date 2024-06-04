1867dd038971c192582befc36bfb9097b04f2339
Enzo <118602062+enzomoy@users.noreply.github.com>
2024-06-04 17:04:21 +0200

    add api request on login form & redirection

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 const port: number = Number.parseInt(process.env.ONYXYA_API_PORT) | 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuration de CORS
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(port);
 }
 
