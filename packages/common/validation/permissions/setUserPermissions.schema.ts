4ea10cd679292b3e166ef2f1af0ae399e39aff8b
L4p1s <124094769+PommePain@users.noreply.github.com>
2024-06-22 12:41:43 +0200

    fix reload and set perms

--- packages/common/validation/permissions/setUserPermissions.schema.ts
+++ packages/common/validation/permissions/setUserPermissions.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const setUserPermissionsSchema = z.object({
   userId: z.number({ message: 'User Id must be a number' }),
-  permissions: z.number().array().min(1),
+  permissions: z.number().array(),
 }).refine((data) => data !== undefined, {
   message: "Need user's and permission's list",
   path: [],
