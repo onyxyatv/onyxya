a5bd628422416d01dcb29a7fbebfa52d2e016eb4
L4p1s <124094769+PommePain@users.noreply.github.com>
2024-06-28 13:55:13 +0200

    eslint fixes

--- packages/frontend/src/components/music/musicsList.tsx
+++ packages/frontend/src/components/music/musicsList.tsx
@@ -3,13 +3,14 @@ import { AxiosResponse, HttpStatusCode } from "axios";
 import { useEffect, useState } from "react";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 import { AlertCircle } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface MusicListCategory {
   category: string;
   musics: Array<object>;
 }
 
-const MusicsLists: React.FC = (props: { playMusic: any }) => {
+const MusicsLists = (props: { playMusic: any }) => {
   const [musics, setMusics] = useState([]);
   const [error, setError] = useState('');
 
@@ -39,6 +40,9 @@ const MusicsLists: React.FC = (props: { playMusic: any }) => {
             <div className={`musics-list-${musicList.category}-category`}>
               <h3>{musicList.category}</h3>
               <p>{musicList.musics.length}</p>
+              <Button onClick={() => props.playMusic(2)}>
+
+              </Button>
             </div>
           );
         })
--- packages/frontend/src/pages/Music.tsx
+++ packages/frontend/src/pages/Music.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/header/header";
 import MusicsLists from "@/components/music/musicsList";
-import { Button } from "@/components/ui/button";
 import FrontUtilService from "@/utils/frontUtilService";
 import { useEffect, useState } from "react";
 import AudioPlayer from 'react-h5-audio-player';
@@ -9,8 +8,8 @@ import 'react-h5-audio-player/lib/styles.css';
 const Music = () => {
   const [music, setMusic] = useState('');
 
-  const fetchMusic = (async () => {
-    const endpoint: string = '/media/getFile/2';
+  const fetchMusic = (async (musicId: number) => {
+    const endpoint: string = '/media/getFile/' + musicId;
     const res: Blob = await FrontUtilService.getBlobFromApi(endpoint);
     if (res.size > 0) {
       const url = URL.createObjectURL(res);
@@ -26,7 +25,7 @@ const Music = () => {
       <Header />
 
       <section>
-        <MusicsLists playMusic={fetchMusic} />
+        <MusicsLists playMusic={(musicId: number) => fetchMusic(musicId)} />
       </section>
 
       <section className="bg-blue-400 p-2 flex flex-row fixed bottom-0 w-full justify-center">
